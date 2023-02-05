08d91f00ea133179a8514c6828c25f528890c98c
Alex Clevenger <alexbclevenger@gmail.com>
2023-02-05 17:26:53 -0500

    added comment

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-# build script
+# build script --> needs modification for ubuntu environment
+#
+#
+#
 c++ -O3 -Wall -shared -std=c++11 $(python3 -m pybind11 --includes) functions.cc -o functions$(python3-config --extension-suffix)
