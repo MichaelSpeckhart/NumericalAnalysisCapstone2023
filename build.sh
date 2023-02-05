b8983ebfaeab39d421f813f3144c6c873b39c959
Alex Clevenger <alexbclevenger@gmail.com>
2023-02-05 17:05:51 -0500

    removed OSX flag

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # build script
-c++ -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup $(python3 -m pybind11 --includes) functions.cc -o functions$(python3-config --extension-suffix)
+c++ -O3 -Wall -shared -std=c++11 $(python3 -m pybind11 --includes) functions.cc -o functions$(python3-config --extension-suffix)
