8b2fee85f4fda5bb9264baf4b114414947242fb2
Alex Clevenger <alexbclevenger@gmail.com>
2023-02-06 11:17:50 -0500

    fixed build script

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 #
 #
 #
-c++ -O3 -Wall -shared -std=c++11 $(python3 -m pybind11 --includes) functions.cc -o functions$(python3-config --extension-suffix)
+#c++ -O3 -Wall -shared -std=c++11 $(python3 -m pybind11 --includes) functions.cc -o functions$(python3-config --extension-suffix)
+
+#modified compilation line for ubuntu
+g++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` functions.cc -o functions$(python3-config --extension-suffix)
