5f603b110f200907c9e5aca5de534990253927ae
lsh224 <lsh224@lehigh.edu>
2023-02-24 23:52:50 -0500

    made method to simplify testing

--- BackEnd/UnitTesting/docTest.cc
+++ BackEnd/UnitTesting/docTest.cc
@@ -4,31 +4,31 @@
 //This file just defines are Doctest config
 
 
-TEST_CASE("testing CSR") {
+void CHECKCSR(CSRMatrix<int> mResult, vector<vector<int> > mCheck){
+    CHECK(mResult.numRows == mCheck.size());
+    for(size_t i = 0; i < mResult.numRows;i++){
+        CHECK(mResult.numColumns == mCheck[i].size());
+        for(size_t j = 0; j< mResult.numColumns;j++){
+             CHECK(get_matrixCSR(mResult, i, j) == mCheck[i][j]);
+        }
+    }
+}
+
+TEST_CASE("testing CSR Add") {
     vector<vector<int> > array = {{1,0,0},{4,5,6},{0,8,9}};
 
     vector<vector<int> > array2 = {{0,2,3},{0,-5,0},{7,8,0}};
 
     CSRMatrix<int> m1 = from_vector<int>(array);
     //check m1
-    CHECK(get_matrixCSR(m1, 0, 0) == 1);
-    CHECK(get_matrixCSR(m1, 1, 0) == 4);
-    CHECK(get_matrixCSR(m1, 2, 1) == 8);
+    CHECKCSR(m1,array);
 
     CSRMatrix<int> m2 = from_vector<int>(array2);
     //check m2
-    CHECK(get_matrixCSR(m2, 0, 1) == 2);
-    CHECK(get_matrixCSR(m2, 1, 1) == -5);
-    CHECK(get_matrixCSR(m2, 2, 1) == 8);
+    CHECKCSR(m2,array2);
+
     CSRMatrix<int> m3 = add_matrixCSR<int>(m1, m2);
+    vector<vector<int> > addResultExpected = {{1,2,3},{4,0,6},{7,16,9}};
     //check the addition
-    CHECK(get_matrixCSR(m3, 0, 0) == 1);
-    CHECK(get_matrixCSR(m3, 0, 1) == 2);
-    CHECK(get_matrixCSR(m3, 0, 2) == 3);
-    CHECK(get_matrixCSR(m3, 1, 0) == 4);
-    CHECK(get_matrixCSR(m3, 1, 1) == 0);
-    CHECK(get_matrixCSR(m3, 1, 2) == 6);
-    CHECK(get_matrixCSR(m3, 2, 0) == 7);
-    CHECK(get_matrixCSR(m3, 2, 1) == 16);
-    CHECK(get_matrixCSR(m3, 2, 2) == 9);
+    CHECKCSR(m3,addResultExpected);
 }
