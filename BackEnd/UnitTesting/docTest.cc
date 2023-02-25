cf942de2695a25c3ed54b661d385ca1c6917b6d2
lsh224 <lsh224@lehigh.edu>
2023-02-24 20:43:16 -0500

    unit testing framework

--- BackEnd/UnitTesting/docTest.cc
+++ BackEnd/UnitTesting/docTest.cc
@@ -1,3 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 //This file just defines are Doctest config
+
+int factorial(int number) { return number > 1 ? factorial(number - 1) * number : 1; }
+
+TEST_CASE("testing the factorial function") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+}
