aee58ec082bc820abdf1d27658b124ca3b4dbf0d
lsh224 <lsh224@lehigh.edu>
2023-04-09 22:15:09 -0400

    better comparison of parallel vs serial

--- BackEnd/UnitTesting/doctestParallel.cc
+++ BackEnd/UnitTesting/doctestParallel.cc
@@ -1,36 +1,50 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "../functionsCSRParallel.cc"
-#include "../functionsCSR.cc"
 #include "fstream"
 //Basic Unit tests for CSR add, multiply, and transpose
 //Use -d to time the tests
 
 /*
-Takes a CSRMatrix and a dense matrix and makes sure they have the same elements
+Ensure two CSR matrices are the same
 */
-/// @brief Ensure compressed sparse row matrix is the same as expected dense matrix
-/// @param mResult The CSR matrix to compare to
-/// @param mCheck The dense matrix to compare to
-// void CHECKCSR(CSRMatrix<int> mResult, vector<vector<int> > mCheck){
-//     CHECK(mResult.numRows == mCheck.size());
-//     for(size_t i = 0; i < mResult.numRows;i++){
-//         CHECK(mResult.numColumns == mCheck[i].size());
-//         for(size_t j = 0; j< mResult.numColumns;j++){
-//              CHECK_MESSAGE(get_matrixCSR(mResult, i, j) == mCheck[i][j],"i = "<< i <<", j = "<<j);
-//         }
-//     }
-// }
+void CHECKCSR(CSRMatrix<double> one, CSRMatrix<double> two){
+    CHECK(one.numRows == two.numRows);
+    CHECK(one.numColumns == two.numColumns);
+    for(size_t i = 0; i < one.numRows;i++){
+        for(size_t j = 0; j< one.numColumns;j++){
+             CHECK_MESSAGE(get_matrixCSR(one, i, j) == get_matrixCSR(two, i, j),"i = "<< i <<", j = "<<j);
+        }
+    }
+}
+
+TEST_CASE("CSR ADD parallel Correctness") {
+
+    CSRMatrix<double> m1 = load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
+    CSRMatrix<double> m2 = load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
+
+    CSRMatrix<double> m3 = add_matrixCSR<double>(m1, m2);
+    CSRMatrix<double> m4 = parallel::add_matrixCSR<double>(m1, m2);
+    CHECKCSR(m3,m4);
+}
 
 
-TEST_CASE("testing CSR on real matrix") {
+TEST_CASE("CSR ADD parallel TIME") {
 
-    parallel::CSRMatrix<double> m1 = parallel::load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
+    CSRMatrix<double> m1 = load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
 
-    parallel::CSRMatrix<double> m2 = parallel::load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
+    CSRMatrix<double> m2 = load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
     //for(size_t i=0 ; i < 1000;i++){
-    parallel::CSRMatrix<double> m3 = parallel::add_matrixCSR<double>(m1, m2);
+    CSRMatrix<double> m3 = parallel::add_matrixCSR<double>(m1, m2);
     //}
+}
 
+TEST_CASE("CSR ADD serial TIME") {
 
+    CSRMatrix<double> m1 = load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
+
+    CSRMatrix<double> m2 = load_fileCSR<double>("../../data/matrices/1138_bus.mtx");
+    //for(size_t i=0 ; i < 1000;i++){
+    CSRMatrix<double> m3 = add_matrixCSR<double>(m1, m2);
+    //}
 }
