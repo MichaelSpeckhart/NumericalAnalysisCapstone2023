a477db7c2ce966b6f10889fd042500379c63c655
lsh224 <lsh224@lehigh.edu>
2023-11-15 10:42:11 -0500

    more graphing

--- scripts/ExampleGraphs.py
+++ scripts/ExampleGraphs.py
@@ -7,28 +7,30 @@ if not os.path.exists('graphs'):
     os.makedirs('graphs')
 
 #threads = 2
-size = [1000,1500,2000,2500,3000,3500,4000,4500]
+size = [500,1000,1500,2000,2500,3000,3500,4000]
 
-jacobi = [0.759125,1.82483,3.27048,5.11037,7.40779,10.135,13.2603,16.7436]
-gauss_elimination = [0.0643407,0.24656,0.777767,1.74874,3.27369,5.46504,8.42173,12.3634]
+jacobi = [0.184565,0.759483,1.82614,3.27084,5.12254,7.39125,10.0981,13.2249]
+ssor10 = [0.373677,1.53101,3.62324,6.71153,10.5258,15.151,20.6353,26.8876]
+gauss_sidel = [0.156668,0.69557,1.70365,3.12661,4.96193,7.21092,9.8265,12.9168]
+gauss_elimination = [0.00811063,0.0642115,0.245409,0.7746,1.71589,3.13128,5.42308,8.46707]
 
 #Set the figure size and font size
 plt.figure(figsize=(8, 8))
 plt.rcParams.update({'font.size': 12})
 
 # Plot the data with a solid line and marker
+plt.plot(size, ssor10, ':^', label="SSOR w = 1.0", color='orange')
 plt.plot(size, jacobi, '-o', label="Jacobi Method", color='red')
+plt.plot(size, gauss_sidel, '-.D', label="Gauss Sidel", color='blue')
 plt.plot(size, gauss_elimination, '--s', label="Gauss Eliminaton", color='yellow')
-# plt.plot(size, vector, '-.D', label="Vector", color='blue')
-# plt.plot(size, noPivot, ':^', label="No Pivot", color='orange')
-# plt.plot(size, rayonNoPivot, ':*', label="Rayon+NoPivot", color='purple')
+#plt.plot(size, ssor15, ':*', label="SSOR w = 1.5", color='purple')
 # plt.plot(size, rayonVectorNoPivot, '-.+', label="Rayon+Vector+NoPivot", color='green')
 
 # Add axis labels and title
 plt.xlabel('Size of Coefficient Matrix')
 plt.ylabel('Execution Time (s)')
 plt.suptitle("Size of System Vs. Execution Time")
-plt.title("A is tridiagonal, B is first canonical basis vector")
+plt.title("A is Tridiagonal, B is First Canonical Basis Vector, Dense Data Structure")
 
 # Add gridlines and legend
 plt.grid(False)
